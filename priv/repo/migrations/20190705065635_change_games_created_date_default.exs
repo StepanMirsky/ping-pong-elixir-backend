c3dd05c6c4e88316f9e7df88be220d2d1825627f
Мирский Степан Алексеевич <samirskiy@domclick.ru>
2019-07-05 13:07:26 +0300

    Removed broken migration file

--- priv/repo/migrations/20190705065635_change_games_created_date_default.exs
+++
@@ -1,9 +0,0 @@
-defmodule PingPongElixir.Repo.Migrations.ChangeGamesCreatedDateDefault do
-  use Ecto.Migration
-
-  def change do
-    alter table(:games) do
-      modify :date_created, null: false
-    end
-  end
-end
